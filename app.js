c0d83b5578b7339cab17344cdc52582bbf46c476
bcdyas <66280720+bcdyas@users.noreply.github.com>
2021-09-25 20:06:55 -0700

    updated listen port for heroku

--- app.js
+++ app.js
@@ -21,4 +21,4 @@ app.use((req, res, next) => {
    res.status(404).render('404', { pageTitle: 'Page Not Found' }); //0:36 working with ejs
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
