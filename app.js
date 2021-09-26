ee86a7e1b3c40abbfd0b07e677bed9930a1b4b5b
bcdyas <66280720+bcdyas@users.noreply.github.com>
2021-09-25 17:23:24 -0700

    (Done) Working with Express.js

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-//OPTION 1
-// const server = http.createServer(routes);
+const app = express();
 
-//OPTION 2
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+//const routes = require('./routes');
+//console.log(routes.someText);
+//const server = http.createServer(routes.handler);
+//const server = http.createServer(app);
 
 
-server.listen(3000);
+app.listen(3000);
