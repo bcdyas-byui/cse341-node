54a27f36a27b1b2e13cf7f7bd91d38c5928c1377
bcdyas <66280720+bcdyas@users.noreply.github.com>
2021-09-28 19:52:54 -0700

    (done) Dynamic Roles/Advanced Models

--- models/cart.js
+++ models/cart.js
@@ -43,11 +43,14 @@ module.exports = class Cart {
          }
          const updatedCart = { ...JSON.parse(fileContent) };
          const product = updatedCart.products.findIndex(prod => prod.id === id);
+         if (!product) {
+            return;
+         }
          const productQty = product.qty;
          updatedCart.products = updatedCart.products.filter(
             prod => prod.id !== id
          );
-         updatedCart.totalPrice = cart.totalPrice - productPrice * productQty;
+         updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty; //producing null if cart is empty
 
          fs.writeFile(p, JSON.stringify(updatedCart), err => {
             console.log(err);
