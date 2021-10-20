749f8a189cc7cb530511ffcb8c287ec7a4d8c435
bcdyas <66280720+bcdyas@users.noreply.github.com>
2021-10-19 19:13:57 -0700

    (done) Understanding Validation

--- controllers/shop.js
+++ controllers/shop.js
@@ -90,7 +90,7 @@ exports.getCart = (req, res, next) => {
       });
       const order = new Order({
          user: {
-            name: req.user.name,
+            email: req.user.email,
             userId: req.user
          },
          products: products
