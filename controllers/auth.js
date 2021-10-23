41fb09e8ca47549620a97d482d0b3d3464c05a65
bcdyas <66280720+bcdyas@users.noreply.github.com>
2021-10-23 11:18:01 -0700

    (done) Error Handling

--- controllers/auth.js
+++ controllers/auth.js
@@ -114,7 +114,11 @@ exports.postLogin = (req, res, next) => {
           res.redirect('/login');
         });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+   });
 };
 
 exports.postSignup = (req, res, next) => {
@@ -156,8 +160,10 @@ exports.postSignup = (req, res, next) => {
       });
     })
     .catch(err => {
-      console.log(err);
-    });
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+   });
  };
 
 exports.postLogout = (req, res, next) => {
@@ -211,8 +217,10 @@ exports.postReset = (req, res, next) => {
         });
       })
       .catch(err => {
-        console.log(err);
-      });
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+     });
   });
 };
 
@@ -235,8 +243,10 @@ exports.getNewPassword = (req, res, next) => {
       });
     })
     .catch(err => {
-      console.log(err);
-    });
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+   });
 };
 
 exports.postNewPassword = (req, res, next) => {
@@ -264,6 +274,8 @@ exports.postNewPassword = (req, res, next) => {
       res.redirect('/login');
     })
     .catch(err => {
-      console.log(err);
-    });
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+   });
 };
